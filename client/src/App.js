e39b1ff22332dc02f016e5505f5dd9676f8d6f37
juliang <36992361+juliangalvez@users.noreply.github.com>
2022-10-10 20:58:49 -0300

    Add new front routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Home from "./pages/Home";
 import Operations from "./pages/Operations";
 import OperationForm from "./pages/OperationForm";
 import Navbar from "./components/Navbar";
+import { AppProvider } from "./AppContext";
+import styled from "styled-components";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/operations" component={Operations} />
-        <Route exact path="/new" component={OperationForm} />
-      </Switch>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Container>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/operations" component={Operations} />
+            <Route exact path="/new" component={OperationForm} />
+          </Container>
+        </Switch>
+      </BrowserRouter>
+    </AppProvider>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  @media (min-width: 600px) {
+    margin: auto;
+    width: 600px;
+  }
+  @media (max-width: 400px) {
+    margin: auto;
+    width: 380px;
+  }
+`;
