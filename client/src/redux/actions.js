926de1d62724e942aef6fd7b9dcfd2d297545dfa
juliang <36992361+juliangalvez@users.noreply.github.com>
2022-10-10 21:01:17 -0300

    Add redux actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,15 +1,42 @@
 import axios from "axios";
-import {
-  GET_OPERATIONS,
-} from "./constants";
+import { GET_OPERATIONS } from "./constants";
 
 export const getOperations = () => {
   return async (dispatch) => {
     try {
-      let results = await axios.get("/operations");
+      let results = await axios.get("http://localhost:3001/operations");
       dispatch({ type: GET_OPERATIONS, payload: results.data });
     } catch (error) {
-      console.error(error)
+      console.error(error);
+    }
+  };
+};
+
+export const postOperation = (payload) => {
+  return async () => {
+    try {
+      await axios.post("http://localhost:3001/operations/add", payload);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+export const editOperation = (payload) => {
+  return async () => {
+    try {
+      await axios.patch("http://localhost:3001/operations/edit", payload);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+export const delOperation = (payload) => {
+  return async () => {
+    try {
+      console.log(payload)
+      await axios.delete("http://localhost:3001/operations/delete", { data: payload });
+    } catch (error) {
+      console.error(error);
     }
   };
 };
@@ -20,4 +47,3 @@ export const getOperations = () => {
 //     payload,
 //   };
 // }
-
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -3,18 +3,28 @@ import { GET_OPERATIONS } from "./constants";
 let initialState = {
   allOperations: [],
   operations: [],
+  balance: "",
 };
 
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
     case GET_OPERATIONS:
+      function dateSorter(a, b) {
+        if (a.date < b.date) return 1;
+        else if (a.date > b.date) return -1;
+        else return 0;
+      }
+
+      let sortOps = action.payload.results.sort(dateSorter);
+      let homeOps = sortOps.slice(0, 10);
+
       return {
         ...state,
-        allOperations: action.payload,
-        operations: action.payload,
+        allOperations: sortOps,
+        operations: homeOps,
+        balance: action.payload.balance
       };
 
-      
     default:
       return state;
   }
